0f3edd987cd8d7041b9066bdf3c450224d6f1fd9
space-commits <57108281+space-commits@users.noreply.github.com>
2022-07-21 21:07:15 +0100

    Initial commit

---
+++ Patch.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Reflection;
+using Aki.Reflection.Patching;
+using EFT.InventoryLogic;
+
+namespace InspectionlessMalfs
+{
+	public class KnowMalf : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(Weapon.GClass2092).GetMethod("IsKnownMalfType", BindingFlags.Instance | BindingFlags.Public);
+		}
+		[PatchPostfix]
+		private static void PatchPostfix(ref bool __result)
+		{
+			__result = true;
+		}
+	}
+}
