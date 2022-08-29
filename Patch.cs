9e3380133b5a55814945dfd911cec78ebd61adaa
space-commits <57108281+space-commits@users.noreply.github.com>
2022-08-29 23:01:49 +0100

    Updated to 3.2.2 Client

--- Patch.cs
+++ Patch.cs
@@ -9,7 +9,7 @@ namespace InspectionlessMalfs
 	{
 		protected override MethodBase GetTargetMethod()
 		{
-			return typeof(Weapon.GClass2177).GetMethod("IsKnownMalfType", BindingFlags.Instance | BindingFlags.Public);
+			return typeof(Weapon.GClass2189).GetMethod("IsKnownMalfType", BindingFlags.Instance | BindingFlags.Public);
 		}
 		[PatchPostfix]
 		private static void PatchPostfix(ref bool __result)
